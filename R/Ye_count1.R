979381d9a3355d37ab4bdf7b903168720962f11c
yqzhu <zyq11071107@gmail.com>
2025-11-24 06:26:33 -0500

    Add files via upload

---
+++ R/Ye_count1.R
@@ -0,0 +1,13 @@
+#' Simulated OTU count table
+#'
+#' A processed OTU count table contains 44 subjects with 304 OTUs.
+#'
+#' @usage data("Ye_count1")
+#'
+#' @format A data frame with 44 observations on 304 variables.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(Ye_count1)
+"Ye_count1"
